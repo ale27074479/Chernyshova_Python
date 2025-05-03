1a0bb934aaa77624e2bfb8f317f05f0a939926d7
ale27074479 <87765881+ale27074479@users.noreply.github.com>
2025-05-03 19:17:27 +0300

    Update lesson_1_task_6.py

--- lesson_01/lesson_1_task_6.py
+++ lesson_01/lesson_1_task_6.py
@@ -1,9 +1,9 @@
 my_height = 160
 print(my_height)
 
-my_name = "Alex"
-my_name = "Alex Chernyshova"
-print(my_name)
+first_name = input("Введите имя: ")
+last_name = input("Введите фамилию: ")
+print(f"Вас зовут: {last_name} {first_name}")
 
 pet_name = input("Как зовут вашего питомца? ")
 print("Ваш любимчик - " + pet_name)
